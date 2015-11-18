89cb7cd094a3637e3c934af46a7f2a4e7db4cb3e
devendradhanal <devendradhanal@gmail.com>
2015-11-18 15:17:12 +0530

    Adding conext to repl

--- repl.js
+++ repl.js
@@ -1,12 +1,14 @@
 var repl = require('repl');
-
+var _ = require('lodash');
 
 var replServer = repl.start({
 	prompt: '#!',
 	input: process.stdin,
     output: process.stdout,
-	eval : my_custom_eval_fn
+//	eval : my_custom_eval_fn
 });
+
+replServer.context._ = _;
 
 replServer.defineCommand('print', {
   help: 'print <text here>',
